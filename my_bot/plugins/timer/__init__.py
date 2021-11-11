8fa24b04808094507ad1b9be2882ddac02a754a8
youremailaddress <3497511332@qq.com>
2021-11-11 19:56:51 +0800

    reset to needed time

--- my_bot/plugins/timer/__init__.py
+++ my_bot/plugins/timer/__init__.py
@@ -12,7 +12,7 @@ import json
 
 scheduler = require("nonebot_plugin_apscheduler").scheduler
 
-@scheduler.scheduled_job("cron", hour="19",minute="54")
+@scheduler.scheduled_job("cron", hour="9",minute="50")
 async def tm():
     bot = get_bot()
     group_id=744129478
