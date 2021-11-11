3141b917fb3f948315deca3bc8b72d20fc57a2fb
youremailaddress <3497511332@qq.com>
2021-11-11 19:48:47 +0800

    1

--- my_bot/plugins/timer/__init__.py
+++ my_bot/plugins/timer/__init__.py
@@ -12,7 +12,7 @@ import json
 
 scheduler = require("nonebot_plugin_apscheduler").scheduler
 
-@scheduler.scheduled_job("cron", hour="19",minute="48")
+@scheduler.scheduled_job("cron", hour="19",minute="50")
 async def tm():
     bot = get_bot()
     # group_id=744129478
