f480c66b2c228b6e38f2e62cef24ff862b1ade76
youremailaddress <youremailaddress@github.io>
2022-07-21 16:47:43 +0800

    test bililive

--- my_bot/plugins/timer/__init__.py
+++ my_bot/plugins/timer/__init__.py
@@ -1,5 +1,6 @@
 from nonebot import require, get_bot
 from .dailynews import news
+from .bililive import handleblive
 import datetime
 
 sche = require("nonebot_plugin_apscheduler").scheduler
@@ -10,5 +11,6 @@ async def sche_():
     if datetime.datetime.now().hour == 9 and datetime.datetime.now().minute == 50:
         for gi in group_id:
             await news(bot,gi)
+        await handleblive(bot)
 
 
--- my_bot/plugins/timer/bililive.py
+++ my_bot/plugins/timer/bililive.py
@@ -1,4 +1,11 @@
-import requests
+import requests,time
+from ..bililivereminder.data_source import BiliLiveDBHandler
+from nonebot.adapters.cqhttp.message import Message
+bilidb = BiliLiveDBHandler()
+
+def gettimestamp(timestr):
+    return int(time.mktime(time.strptime(timestr,'%Y-%m-%d %H:%M:%S')))
+
 def getbililive(uid):
     resp = requests.get(f"https://api.live.bilibili.com/xlive/web-room/v1/index/getRoomBaseInfo?uids={uid}&;req_biz=video")
     js = resp.json()
@@ -6,7 +13,21 @@ def getbililive(uid):
         return None
     else:
         m = js['data']['by_uids'][str(uid)]
-        return m["title"],m['live_url'],m['cover'],m['uname']
+        return m["title"],m['live_url'],m['cover'],m['uname'],m["live_time"]
 
-def handleblive():
-    pass
+async def handleblive(bot):
+    dic = bilidb.fetch_blive()
+    for k,v in dic.items():
+        resp = getbililive(k)
+        if resp == None:
+            continue
+        title,liveurl,cover,uname,livetime = resp
+        for config in v:
+            if int(time.time())-gettimestamp(livetime)>60:
+                continue
+            elif config["ingroup"] == 0:
+                msg = Message(f"你关注的{uname}主播开播啦，直播名字{title}，直播地址{liveurl}")
+                await bot.send_private_msg(user_id=config["user"],message=msg+Message(f'[CQ:image,file={cover}]'))
+            else:
+                msg = Message(f"你关注的{uname}主播开播啦，直播名字{title}，直播地址{liveurl}")
+                await bot.send_group_msg(group_id=config["group"], message=msg+Message(f'[CQ:image,file={cover}]'))
