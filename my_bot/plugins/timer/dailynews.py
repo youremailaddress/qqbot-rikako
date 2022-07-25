261fe71737399ea6fecbccb3c4975c82f2fc84e8
youremailaddress <youremailaddress@github.com>
2022-07-25 16:11:01 +0800

    fix stupid bugs =.=

--- my_bot/plugins/timer/dailynews.py
+++ my_bot/plugins/timer/dailynews.py
@@ -1,11 +1,11 @@
 from nonebot.adapters.cqhttp.message import Message
 import datetime
-year = datetime.datetime.now().year
-month = datetime.datetime.now().month
-day = datetime.datetime.now().day
 async def news(bot,group_id):
     try:
         try:
+            year = datetime.datetime.now().year
+            month = datetime.datetime.now().month
+            day = datetime.datetime.now().day
             img_url = f'https://raw.githubusercontent.com/pkupersonalities/Keji/main/output/img/news_{year}_{month}_{day}.jpg'
             cq = "[CQ:image,file="+img_url+"]"
             msg=Message(cq)
