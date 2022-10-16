5b9c80d66743c0f00339cc187fbb8e101786c735
youremailaddress <youremailaddress@github.io>
2022-10-17 02:30:51 +0800

    fix reminder bugs

--- my_bot/plugins/reminder/remind.py
+++ my_bot/plugins/reminder/remind.py
@@ -4,7 +4,7 @@ from utils.multimediahandler import makeAt
 import time
 @tmr.checker(info="remind sb sth")
 async def reminder(bot:Bot,uid,gid,**kwargs):
-    if uid == "*":
+    if uid == None:
         msg = f"[一条来自{makeAt(kwargs['sender'])}在{time.strftime('%Y/%m/%d %H:%M',time.localtime(kwargs['sendtime']))}设置的提醒消息]{kwargs['msg']}"
         await bot.send_group_msg(group_id=gid,message=msg)
     else:
