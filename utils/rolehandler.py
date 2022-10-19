76812c6134357aac477b7524731c8a58b1c70e06
youremailaddress <youremailaddress@github.io>
2022-10-19 17:17:18 +0800

    fix & role problem

--- utils/rolehandler.py
+++ utils/rolehandler.py
@@ -3,15 +3,21 @@ driver = get_driver()
 global_config = get_driver().config
 from nonebot.adapters.cqhttp import Bot, Event
 from nonebot.adapters.cqhttp.event import PokeNotifyEvent,FriendRequestEvent
-from nonebot.rule import Rule
 
-async def is_on(bot:Bot,event:Event,_):
+def handleRule(bot:Bot,event:Event,_,*func):
+    is_true = True
+    res = [item(bot,event,_) for item in func]
+    if False in res:
+        is_true = False
+    return is_true
+
+def is_on(bot:Bot,event:Event,_):
     '''
     bot开机
     '''
     return global_config.set_on
 
-async def _is_poke(bot:Bot,event:Event,_):
+def _is_poke(bot:Bot,event:Event,_):
     '''
     bot 被 戳一戳
     '''
@@ -21,10 +27,10 @@ async def _is_poke(bot:Bot,event:Event,_):
     return False
 
 async def is_poke(bot:Bot,event:Event,_):
-    return Rule(is_on,_is_poke)
+    return handleRule(bot,event,_,is_on,_is_poke)
 
 async def _is_add_friend(bot:Bot,event:Event,_):
     return isinstance(event,FriendRequestEvent)
 
 async def is_add_friend(bot:Bot,event:Event,_):
-    return Rule(is_on,_is_add_friend)
+    return handleRule(bot,event,_,is_on,_is_add_friend)
