234fb52fb5d17853e4215f5168c0a19db77b692e
youremailaddress <youremailaddress@github.io>
2022-10-20 19:23:22 +0800

    test pic

--- my_bot/plugins/test/__init__.py
+++ my_bot/plugins/test/__init__.py
@@ -13,4 +13,4 @@ matcher = on_command("测试超管",priority=1, permission=SUPERUSER)
 @matcher.handle()
 async def _(bot: Bot, event: Event):
     # await matcher.send(makePic("https://cdn.statically.io/gh/pkupersonalities/Keji/main/output/img/news_2022_10_11.jpg",type="show"))
-    await matcher.send(makeCardImage("https://cdn.jsdelivr.net/npm/chenyfan-happypic@0.0.3/11.jpg"))
+    await matcher.send(makeCardImage("https://cdn.jsdelivr.net/npm/chenyfan-happypic@0.0.3/12.jpg"))
